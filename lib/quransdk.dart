e09fe7c90e6483f7451a90d05551372f14137683
junaid <junaid.khokhar@vaudience.net>
2022-09-01 02:34:59 +0500

    initial commit

---
+++ lib/quransdk.dart
@@ -0,0 +1,6 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library quransdk;
+
+export './src/quransdk_base.dart';
---
+++ lib/src/quransdk_base.dart
@@ -0,0 +1,8 @@
+
+import 'package:quransdk/src/utils/services.dart';
+
+class QuranSdk {
+  QuranSdk() {
+    initServices();
+  }
+}
